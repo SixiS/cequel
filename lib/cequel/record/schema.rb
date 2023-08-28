fcf7cd10e4e19fdea56afa9eae08f4212f0503f8
Matthew Hirst <hirst.mat@gmail.com>
2023-08-28 13:12:24 +0200

    Add vector and custom index.

--- lib/cequel/record/schema.rb
+++ lib/cequel/record/schema.rb
@@ -154,6 +154,11 @@ module Cequel
           dsl.map(name, key_type, value_type)
         end
 
+        def vector(name, type, dimensions, options = {})
+          super
+          dsl.vector(name, type, dimensions, options)
+        end
+
         def table_property(name, value)
           dsl.with(name, value)
         end
